568ce8e3412c1d8b666a487aa2612b397a577772
nahid <nahidrabbi045@gmail.com>
2024-03-22 07:04:22 +0600

    add flags button daynamically

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css';
-const Country = ({country, handleVisitedCountry}) => {
+const Country = ({country, handleVisitedCountry, handleVisitedFlags }) => {
     const {name, flags, population, area, cca3} = country;
 
     const [visited, setVisited] = useState(false);
@@ -12,12 +12,14 @@ const Country = ({country, handleVisitedCountry}) => {
    
      return (
         <div className={`country ${visited ? 'visited': 'non-visited'}`}>
-            <h3 style={{color: visited ? 'purple': 'white'}}>{name.common}</h3>
+            <h3 style={{color: visited ? 'purple': 'black'}}>{name.common}</h3>
             <img src={flags.png} alt="" />
             <p>population:{population}</p>
             <p>area:{area}</p>
             <p><small>Code: {cca3}</small></p>
             <button onClick={() => handleVisitedCountry(country)}>Mark visited</button>
+            <br />
+            <button onClick={() => handleVisitedFlags(country.flags.png)} >Add Flag</button>
             <br />
             <button onClick= {handleVisited}>{visited ? 'visited' : 'Going'}</button>
             {visited ? 'I have visited this country.' : 'I want to visit'}
